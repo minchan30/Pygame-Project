37c0942d23485745eb06f092adba2e5319db7437
minchan30 <goalie0901@gmail.com>
2023-11-11 10:40:06 +0900

    :memo:update

--- pygame/app.py
+++ pygame/app.py
@@ -16,26 +16,34 @@ yspeed = 0.01
 gravity = 0.0005
 pos_easing = 0.003
 rad = 20
-red = (255 , 0 , 0)
-blue = (0 , 0 , 255)
-green = (0 , 255 , 0)
-color = red
+
+red = (255, 0, 0)
+orange = (255, 112, 18)
+yellow = (255, 255, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+navy = (0, 0, 128)
+purple = (255, 0, 255)
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+color = white
 
 #이벤트 루프
 running = True #게임 진행 여부에 대한 변수 True : 게임 진행 중
 while running :
-    screen.fill((250 , 250 , 250))
+    screen.fill((0, 250, 250))
     for event in pygame.event.get() : #이벤트의 발생 여부에 따른 반복문
         if event.type == pygame.QUIT : #창을 닫는 이벤트 발생했는가?
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN :
-            (x , y) = pygame.mouse.get_pos()
+            (x, y) = pygame.mouse.get_pos()
             if temp - length // 2 < x < temp + length // 2 and 570 > y > 500 :
                 color = red
             elif temp * 3 - length // 2 < x < temp * 3 + length // 2 and 570 > y > 500 :
-                color = green
+                color = orange
             elif temp * 5 - length // 2  < x < temp * 5 + length // 2 and 570 > y > 500 :
-                color = blue
+                color = yellow
     
     (targetx, targety) = pygame.mouse.get_pos()
     dx = targetx - posx
@@ -46,11 +54,16 @@ while running :
     temp = screen_width // 6
     length = 70
 
-    pygame.draw.circle(screen,color,(posx,posy) , rad , 5)
-    pygame.draw.rect(screen, red , [temp - length // 2 , 500 , length , length] , 5)
-    pygame.draw.rect(screen, green , [temp * 3 - length // 2 , 500 , length , length] , 5)
-    pygame.draw.rect(screen, blue , [temp * 5 - length // 2 , 500 , length , length] , 5)
-   
+    pygame.draw.circle(screen, color, (posx, posy), rad , 5)
+    pygame.draw.rect(screen, red, [temp - length // 2, 500, length, length], 5)
+    pygame.draw.rect(screen, orange, [temp * 3 - length // 2, 500, length, length], 5)
+    pygame.draw.rect(screen, yellow, [temp * 5 - length // 2, 500, length, length], 5)
+    pygame.draw.rect(screen, green, [temp - length // 2, 300, length, length], 5)
+    pygame.draw.rect(screen, blue, [temp * 3 - length // 2, 300, length, length], 5)
+    pygame.draw.rect(screen, navy, [temp * 5 - length // 2, 300, length, length], 5)
+    pygame.draw.rect(screen, purple, [temp - length // 2, 100, length, length], 5)
+    pygame.draw.rect(screen, white, [temp * 3 - length // 2, 100, length, length], 5)
+    pygame.draw.rect(screen, black, [temp * 5 - length // 2, 100, length, length], 5)
     pygame.display.update()
 
 #pygame 종료
