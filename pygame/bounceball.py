e46a55dc521027fdd082124719f5ba294b3d823d
minchan30 <goalie0901@gmail.com>
2023-11-12 12:32:52 +0900

    :memo:update

--- pygame/bounceball.py
+++ pygame/bounceball.py
@@ -9,9 +9,14 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Game")
 posx = screen_width // 2
 posy = screen_height // 2
+posx2 = screen_width // 4
+posy2 = screen_height // 4
 rad = 40
+rad2 = 20
 xspeed = 0.1
 yspeed = 0.1
+xspeed2 = 0.1
+yspeed2 = 0.1
 
 running = True #게임 진행 여부에 대한 변수 True : 게임 진행 중
 while running :
@@ -22,18 +27,25 @@ while running :
     screen.fill((255, 255, 255))
     posx += xspeed
     posy += yspeed
+    posx2 += xspeed2
+    posy2 += yspeed2
 
-    if posx + rad >= screen_width :
+    if posx + rad >= screen_width or posx - rad <= 0 :
         xspeed *= -1
-    elif posx + rad <= screen_width :
-        xspeed *= -1
-    if posy + rad >= screen_height :
+    if posy + rad >= screen_height or posy - rad <= 0 :
         yspeed *= -1
-    elif posy + rad <= screen_height :
-        yspeed *= -1
-
-
+    if posx2 + rad2 >= screen_width or posx2 - rad2 <= 0 :
+        xspeed2 *= -1
+    if posy2 + rad2 >= screen_height or posy2 - rad2 <= 0 :
+        yspeed2 *= -1
+    D = ((posx - posx2) * (posx - posx2) + (posy - posy2) * (posy - posy2))**0.5
+    if rad + rad2 > D :
+        posx *= -1
+        posy *= -1
+        posx2 *= -1
+        posy2 *= -1
     pygame.draw.circle(screen, (0, 0, 0), (posx, posy), rad)
+    pygame.draw.circle(screen, (0, 0, 0), (posx2, posy2), rad2)
     pygame.display.update()
 
 
